6fae16d957e4d42e0817f4dd6fb218036e1503e7
captain-zhu <andypaker@qq.com>
2016-09-12 17:01:38 +0800

    fix postgres bug

--- config/database.php
+++ config/database.php
@@ -33,7 +33,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
